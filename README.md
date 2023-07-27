56bed8267902f8dee5aecff2db7b610be7d89c39
Debs-lima <deborahalves.lima@gmail.com>
2023-07-27 12:10:45 -0300

    Adição de alerta de erro - favoritos

--- README.md
+++ README.md
@@ -12,6 +12,11 @@ Consumir uma API com JavaScript puro, ou seja, sem nenhum framework ou bibliotec
 Criar um índice de filmes populares, com a possibilidade de favoritá-lo! Nos moldes do layout <a href="https://www.figma.com/file/T6KNbp4XRHNImNFczpHVts/Popular-Movies?type=design&node-id=0%3A1&t=268zBlP4xYh2vhM9-1">Figma</a> disponibilizado.  
 <img align="right" src="./assets/images/joker-movie-joker.gif" width="300" alt="Gif of Joke with the subtitle: Best movie ever!!!">
 
+### OBS
+<hr>
+Fui, ao longo do desafio, implementando uns códigos que para mim fariam sentido para a navegação na aplicação, como por exemplo:
+- alerta de erro quando se quer listar apenas os favoritos, mas não se tem o que listar
+- redirecionamento da para a lista de filmes populares quando não se coloca nenhum input em específico na pesquisa
 
 ### Dia 1
 <hr>
--- services/favoritos.js
+++ services/favoritos.js
@@ -1,4 +1,7 @@
 const checkbox = document.querySelector('#favoritos-apenas');
+const header = document.querySelector('.filtros');
+
+
 import { limparCatalogo, ehFavorito } from "../helpers/helper.js";
 import { pegarFilmesPopulares } from "./api.js";
 import renderizarFilme from "./render.js";
@@ -24,15 +27,15 @@ function removerDoLocalStorage(id) {
 
 /* FAVORITAR */
 function alteraFavorito(evento, filme) {
-  if(ehFavorito(filme.id)) {
+  if (ehFavorito(filme.id)) {
     return desfavoritar(evento, filme);
-  } 
-   return favoritar(evento, filme);
+  }
+  return favoritar(evento, filme);
 }
 
 function desfavoritar(evento, filme) {
-    evento.target.src = '../assets/icons/Heart.svg';
-    removerDoLocalStorage(filme.id);
+  evento.target.src = '../assets/icons/Heart.svg';
+  removerDoLocalStorage(filme.id);
 }
 
 function favoritar(evento, filme) {
@@ -41,22 +44,43 @@ function favoritar(evento, filme) {
 }
 
 /* MOSTRAR APENAS OS FAVORITOS */
-//melhorar caixinha de alerta
+function criaAlerta() {
+  const alertaContainer = document.createElement('div');
+  alertaContainer.classList.add('alerta-container');
+  header.appendChild(alertaContainer);
+  const alerta = document.createElement('p');
+  alerta.classList.add('alerta');
+  alerta.textContent = "You don't have any favorite movie yet!";
+  alertaContainer.appendChild(alerta);
+  const botaoAlerta = document.createElement('button');
+  botaoAlerta.classList.add('botao-alerta');
+  botaoAlerta.textContent = 'OK';
+  alertaContainer.appendChild(botaoAlerta);
+}
+
+function cienteAlerta() {
+  const containerAlerta = document.querySelector('.alerta-container');
+  containerAlerta.remove();
+  pegarFilmesPopulares();
+  checkbox.checked = false;
+}
+
+
 function meusFavoritos() {
-  if(checkbox.checked) {
+  if (checkbox.checked) {
     const listaFilmes = pegarFilmesFavoritos();
-    if(listaFilmes.length == 0) {
-      alert("You don't have any favorite movie yet!");
-      pegarFilmesPopulares();
-      checkbox.checked = false;
-    } 
+    if (listaFilmes.length == 0) {
+      criaAlerta();
+      const alertaBotao = document.querySelector('.botao-alerta');
+      alertaBotao.addEventListener('click', cienteAlerta);
+    }
     limparCatalogo();
     listaFilmes.forEach(filme => renderizarFilme(filme));
     console.log(listaFilmes);
   } else {
     limparCatalogo();
     pegarFilmesPopulares();
-  }  
+  }
 }
 
 export { pegarFilmesFavoritos, alteraFavorito, meusFavoritos }
